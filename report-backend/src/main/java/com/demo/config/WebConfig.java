2f8eee1c71271529c65cc97ced297f903f29b518
wangran326990 <wangrancanada107@gmail.com>
2026-07-25 19:57:56 -0400

    'init'

---
+++ report-backend/src/main/java/com/demo/config/WebConfig.java
@@ -0,0 +1,27 @@
+package com.demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("com.demo")
+public class WebConfig {
+
+    @Bean
+    public InternalResourceViewResolver viewResolver() {
+
+        InternalResourceViewResolver resolver =
+                new InternalResourceViewResolver();
+
+        resolver.setPrefix("/jsp/");
+        resolver.setSuffix(".jsp");
+
+        return resolver;
+    }
+
+}
---
+++ report-backend/src/main/java/com/demo/controller/HomeController.java
@@ -0,0 +1,23 @@
+package com.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class HomeController {
+
+
+    @GetMapping("/home")
+    public String home(Model model) {
+
+        model.addAttribute(
+                "message",
+                "Hello JSP"
+        );
+
+        return "home";
+    }
+
+}
